a39f99fe3162c54adf984c1b01ce9bff122150e2
ItshubhamS <140740558+ItshubhamS@users.noreply.github.com>
2024-05-13 16:14:34 +0530

    commit

--- Coffee_Machine/Main.py
+++ Coffee_Machine/Main.py
@@ -1,54 +1,23 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    },
-}
-profit = 0
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
-def is_resouce_sufficient(order_ingredents):
-    for item in order_ingredents:
-        if order_ingredents[item] >= resources[item]:
-            print(f"sorry there is ot enough {item}")
-            return False
-    return True
+is_on = True
 
-
-while True:
-    choice = input("What do you like to have?(espresso/latte/cappuccino)\n")
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
     if choice == "off":
         is_on = False
     elif choice == "report":
-        print(f"water: {resources['water']}")
-        print(f"milk : {resources['milk']}")
-        print(f"coffee: {resources['coffee']}")
-        print(f"Money: {profit}")
+        coffee_maker.report()
+        money_machine.report()
     else:
-        drink = MENU[choice]
-        if is_resouce_sufficient(drink["ingredients"]):
-            print("enjoy your coffee")
+        drink = menu.find_drink(choice)
+
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
